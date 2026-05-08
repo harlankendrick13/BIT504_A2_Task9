41d752cadf04ce626b74b93e673fadb69b8475ff
Harlan Kendrick <harlankendrick13@gmail.com>
2026-05-08 17:04:53 +1200

    Initial commit - project setup

---
+++ BIT504_A2_Task9/src/BIT504_A2_Task9/Main.java
@@ -0,0 +1,10 @@
+package BIT504_A2_Task9;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Task 9");
+		System.out.println("Print a few lines of code.");
+		System.out.println("Initial Version.");
+	}
+
+}
